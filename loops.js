f6c4d8684659ad3ad2606c02f28d084ded152c0f
alihabibioo <hmidrezar346@gmail.com>
2025-05-09 14:52:59 +0330

    commit one

---
+++ loops.js
@@ -0,0 +1,14 @@
+
+let text="<ul>"
+for (let index = 0; index <= 5; index++) {
+ text+="<li>" +"number "+ index + "</li>"
+}
+text+="</ul>"
+
+document.getElementById("demo").innerHTML=text;
+
+// let firstname= "omid";
+// let lastname="lak";
+//  firstname += lastname;
+// //  firstname = firstname + lastname
+//  console.log(firstname)
